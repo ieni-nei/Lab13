fc6336ad678f4fc052e51104ea5926ed23627d3e
eni <n.golovnenlo@gmail.com>
2026-05-25 17:29:30 +0300

    lab14

--- Lab13/Form1.Designer.cs
+++ Lab13/Form1.Designer.cs
@@ -42,13 +42,32 @@
             btnFilterFiles = new Button();
             txtFolderFilter = new TextBox();
             btnFilterFolders = new Button();
+            menuStrip1 = new MenuStrip();
+            folderToolStripMenuItem = new ToolStripMenuItem();
+            createToolStripMenuItem = new ToolStripMenuItem();
+            deleteToolStripMenuItem = new ToolStripMenuItem();
+            copyToolStripMenuItem = new ToolStripMenuItem();
+            moveToolStripMenuItem = new ToolStripMenuItem();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            createToolStripMenuItem1 = new ToolStripMenuItem();
+            deleteToolStripMenuItem1 = new ToolStripMenuItem();
+            copyToolStripMenuItem1 = new ToolStripMenuItem();
+            moveToolStripMenuItem1 = new ToolStripMenuItem();
+            otherToolStripMenuItem = new ToolStripMenuItem();
+            changeAttributesToolStripMenuItem = new ToolStripMenuItem();
+            saveTextToolStripMenuItem = new ToolStripMenuItem();
+            zIPToolStripMenuItem = new ToolStripMenuItem();
+            uNZIPToolStripMenuItem = new ToolStripMenuItem();
+            txtNewName = new TextBox();
+            label6 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // cmbDrives
             // 
             cmbDrives.FormattingEnabled = true;
-            cmbDrives.Location = new Point(128, 25);
+            cmbDrives.Location = new Point(128, 82);
             cmbDrives.Name = "cmbDrives";
             cmbDrives.Size = new Size(121, 23);
             cmbDrives.TabIndex = 0;
@@ -57,7 +76,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(25, 28);
+            label1.Location = new Point(25, 85);
             label1.Name = "label1";
             label1.Size = new Size(83, 15);
             label1.TabIndex = 1;
@@ -65,16 +84,17 @@
             // 
             // treeViewFolders
             // 
-            treeViewFolders.Location = new Point(25, 146);
+            treeViewFolders.Location = new Point(25, 203);
             treeViewFolders.Name = "treeViewFolders";
-            treeViewFolders.Size = new Size(114, 224);
+            treeViewFolders.Size = new Size(174, 224);
             treeViewFolders.TabIndex = 2;
+            treeViewFolders.BeforeExpand += treeViewFolders_BeforeExpand;
             treeViewFolders.AfterSelect += treeViewFolders_AfterSelect;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(25, 96);
+            label2.Location = new Point(25, 153);
             label2.Name = "label2";
             label2.Size = new Size(114, 15);
             label2.TabIndex = 3;
@@ -84,16 +104,16 @@
             // 
             listBoxFiles.FormattingEnabled = true;
             listBoxFiles.ItemHeight = 15;
-            listBoxFiles.Location = new Point(184, 141);
+            listBoxFiles.Location = new Point(227, 198);
             listBoxFiles.Name = "listBoxFiles";
-            listBoxFiles.Size = new Size(125, 229);
+            listBoxFiles.Size = new Size(172, 229);
             listBoxFiles.TabIndex = 4;
             listBoxFiles.SelectedIndexChanged += listBoxFiles_SelectedIndexChanged;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(184, 96);
+            label3.Location = new Point(207, 153);
             label3.Name = "label3";
             label3.Size = new Size(43, 15);
             label3.TabIndex = 5;
@@ -101,16 +121,16 @@
             // 
             // richTextBoxInfo
             // 
-            richTextBoxInfo.Location = new Point(646, 141);
+            richTextBoxInfo.Location = new Point(659, 198);
             richTextBoxInfo.Name = "richTextBoxInfo";
-            richTextBoxInfo.Size = new Size(122, 137);
+            richTextBoxInfo.Size = new Size(109, 137);
             richTextBoxInfo.TabIndex = 6;
             richTextBoxInfo.Text = "";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(697, 96);
+            label4.Location = new Point(697, 153);
             label4.Name = "label4";
             label4.Size = new Size(71, 15);
             label4.TabIndex = 7;
@@ -118,7 +138,7 @@
             // 
             // txtFileFilter
             // 
-            txtFileFilter.Location = new Point(381, 25);
+            txtFileFilter.Location = new Point(381, 82);
             txtFileFilter.Name = "txtFileFilter";
             txtFileFilter.Size = new Size(97, 23);
             txtFileFilter.TabIndex = 8;
@@ -126,7 +146,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(312, 28);
+            label5.Location = new Point(312, 85);
             label5.Name = "label5";
             label5.Size = new Size(44, 15);
             label5.TabIndex = 9;
@@ -134,15 +154,15 @@
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(381, 141);
+            pictureBox1.Location = new Point(435, 198);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(231, 229);
+            pictureBox1.Size = new Size(205, 229);
             pictureBox1.TabIndex = 10;
             pictureBox1.TabStop = false;
             // 
             // btnFilterFiles
             // 
-            btnFilterFiles.Location = new Point(378, 73);
+            btnFilterFiles.Location = new Point(378, 130);
             btnFilterFiles.Name = "btnFilterFiles";
             btnFilterFiles.Size = new Size(122, 38);
             btnFilterFiles.TabIndex = 11;
@@ -152,14 +172,14 @@
             // 
             // txtFolderFilter
             // 
-            txtFolderFilter.Location = new Point(543, 25);
+            txtFolderFilter.Location = new Point(543, 82);
             txtFolderFilter.Name = "txtFolderFilter";
             txtFolderFilter.Size = new Size(97, 23);
             txtFolderFilter.TabIndex = 12;
             // 
             // btnFilterFolders
             // 
-            btnFilterFolders.Location = new Point(543, 73);
+            btnFilterFolders.Location = new Point(543, 130);
             btnFilterFolders.Name = "btnFilterFolders";
             btnFilterFolders.Size = new Size(122, 38);
             btnFilterFolders.TabIndex = 13;
@@ -167,11 +187,143 @@
             btnFilterFolders.UseVisualStyleBackColor = true;
             btnFilterFolders.Click += btnFilterFolders_Click;
             // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { folderToolStripMenuItem, fileToolStripMenuItem, otherToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 14;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // folderToolStripMenuItem
+            // 
+            folderToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { createToolStripMenuItem, deleteToolStripMenuItem, copyToolStripMenuItem, moveToolStripMenuItem });
+            folderToolStripMenuItem.Name = "folderToolStripMenuItem";
+            folderToolStripMenuItem.Size = new Size(52, 20);
+            folderToolStripMenuItem.Text = "Folder";
+            // 
+            // createToolStripMenuItem
+            // 
+            createToolStripMenuItem.Name = "createToolStripMenuItem";
+            createToolStripMenuItem.Size = new Size(108, 22);
+            createToolStripMenuItem.Text = "Create";
+            createToolStripMenuItem.Click += createToolStripMenuItem_Click;
+            // 
+            // deleteToolStripMenuItem
+            // 
+            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            deleteToolStripMenuItem.Size = new Size(108, 22);
+            deleteToolStripMenuItem.Text = "Delete";
+            deleteToolStripMenuItem.Click += deleteToolStripMenuItem_Click;
+            // 
+            // copyToolStripMenuItem
+            // 
+            copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            copyToolStripMenuItem.Size = new Size(108, 22);
+            copyToolStripMenuItem.Text = "Copy";
+            copyToolStripMenuItem.Click += copyToolStripMenuItem_Click;
+            // 
+            // moveToolStripMenuItem
+            // 
+            moveToolStripMenuItem.Name = "moveToolStripMenuItem";
+            moveToolStripMenuItem.Size = new Size(108, 22);
+            moveToolStripMenuItem.Text = "Move";
+            moveToolStripMenuItem.Click += moveToolStripMenuItem_Click;
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { createToolStripMenuItem1, deleteToolStripMenuItem1, copyToolStripMenuItem1, moveToolStripMenuItem1 });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // createToolStripMenuItem1
+            // 
+            createToolStripMenuItem1.Name = "createToolStripMenuItem1";
+            createToolStripMenuItem1.Size = new Size(108, 22);
+            createToolStripMenuItem1.Text = "Create";
+            createToolStripMenuItem1.Click += createToolStripMenuItem1_Click;
+            // 
+            // deleteToolStripMenuItem1
+            // 
+            deleteToolStripMenuItem1.Name = "deleteToolStripMenuItem1";
+            deleteToolStripMenuItem1.Size = new Size(108, 22);
+            deleteToolStripMenuItem1.Text = "Delete";
+            deleteToolStripMenuItem1.Click += deleteToolStripMenuItem1_Click;
+            // 
+            // copyToolStripMenuItem1
+            // 
+            copyToolStripMenuItem1.Name = "copyToolStripMenuItem1";
+            copyToolStripMenuItem1.Size = new Size(108, 22);
+            copyToolStripMenuItem1.Text = "Copy";
+            copyToolStripMenuItem1.Click += copyToolStripMenuItem1_Click;
+            // 
+            // moveToolStripMenuItem1
+            // 
+            moveToolStripMenuItem1.Name = "moveToolStripMenuItem1";
+            moveToolStripMenuItem1.Size = new Size(108, 22);
+            moveToolStripMenuItem1.Text = "Move";
+            moveToolStripMenuItem1.Click += moveToolStripMenuItem1_Click;
+            // 
+            // otherToolStripMenuItem
+            // 
+            otherToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { changeAttributesToolStripMenuItem, saveTextToolStripMenuItem, zIPToolStripMenuItem, uNZIPToolStripMenuItem });
+            otherToolStripMenuItem.Name = "otherToolStripMenuItem";
+            otherToolStripMenuItem.Size = new Size(49, 20);
+            otherToolStripMenuItem.Text = "Other";
+            // 
+            // changeAttributesToolStripMenuItem
+            // 
+            changeAttributesToolStripMenuItem.Name = "changeAttributesToolStripMenuItem";
+            changeAttributesToolStripMenuItem.Size = new Size(170, 22);
+            changeAttributesToolStripMenuItem.Text = "Change Attributes";
+            changeAttributesToolStripMenuItem.Click += changeAttributesToolStripMenuItem_Click;
+            // 
+            // saveTextToolStripMenuItem
+            // 
+            saveTextToolStripMenuItem.Name = "saveTextToolStripMenuItem";
+            saveTextToolStripMenuItem.Size = new Size(170, 22);
+            saveTextToolStripMenuItem.Text = "Save Text";
+            saveTextToolStripMenuItem.Click += saveTextToolStripMenuItem_Click;
+            // 
+            // zIPToolStripMenuItem
+            // 
+            zIPToolStripMenuItem.Name = "zIPToolStripMenuItem";
+            zIPToolStripMenuItem.Size = new Size(170, 22);
+            zIPToolStripMenuItem.Text = "ZIP";
+            zIPToolStripMenuItem.Click += zIPToolStripMenuItem_Click;
+            // 
+            // uNZIPToolStripMenuItem
+            // 
+            uNZIPToolStripMenuItem.Name = "uNZIPToolStripMenuItem";
+            uNZIPToolStripMenuItem.Size = new Size(170, 22);
+            uNZIPToolStripMenuItem.Text = "UNZIP";
+            uNZIPToolStripMenuItem.Click += uNZIPToolStripMenuItem_Click;
+            // 
+            // txtNewName
+            // 
+            txtNewName.Location = new Point(128, 38);
+            txtNewName.Name = "txtNewName";
+            txtNewName.Size = new Size(100, 23);
+            txtNewName.TabIndex = 15;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(25, 46);
+            label6.Name = "label6";
+            label6.Size = new Size(59, 15);
+            label6.TabIndex = 16;
+            label6.Text = "Нове ім'я";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label6);
+            Controls.Add(txtNewName);
             Controls.Add(btnFilterFolders);
             Controls.Add(txtFolderFilter);
             Controls.Add(btnFilterFiles);
@@ -186,10 +338,14 @@
             Controls.Add(treeViewFolders);
             Controls.Add(label1);
             Controls.Add(cmbDrives);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -210,5 +366,23 @@
         private Button btnFilterFiles;
         private TextBox txtFolderFilter;
         private Button btnFilterFolders;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem folderToolStripMenuItem;
+        private ToolStripMenuItem createToolStripMenuItem;
+        private ToolStripMenuItem deleteToolStripMenuItem;
+        private ToolStripMenuItem copyToolStripMenuItem;
+        private ToolStripMenuItem moveToolStripMenuItem;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem createToolStripMenuItem1;
+        private ToolStripMenuItem deleteToolStripMenuItem1;
+        private ToolStripMenuItem copyToolStripMenuItem1;
+        private ToolStripMenuItem moveToolStripMenuItem1;
+        private ToolStripMenuItem otherToolStripMenuItem;
+        private ToolStripMenuItem changeAttributesToolStripMenuItem;
+        private ToolStripMenuItem saveTextToolStripMenuItem;
+        private ToolStripMenuItem zIPToolStripMenuItem;
+        private ToolStripMenuItem uNZIPToolStripMenuItem;
+        private TextBox txtNewName;
+        private Label label6;
     }
 }
--- Lab13/Form1.cs
+++ Lab13/Form1.cs
@@ -1,3 +1,5 @@
+using System.IO.Compression;
+
 namespace Lab13
 {
     public partial class Form1 : Form
@@ -36,19 +38,52 @@ namespace Lab13
             {
                 string path = node.Tag.ToString();
 
-                string[] dirs = Directory.GetDirectories(path);
-
-                foreach (string dir in dirs)
+                foreach (string dir in Directory.GetDirectories(path))
                 {
-                    TreeNode child = new TreeNode(Path.GetFileName(dir));
+                    TreeNode child =
+                        new TreeNode(Path.GetFileName(dir));
 
                     child.Tag = dir;
+
+                    // fake child
+                    child.Nodes.Add("Loading...");
 
                     node.Nodes.Add(child);
                 }
             }
             catch { }
         }
+
+        private void treeViewFolders_BeforeExpand(
+    object sender,
+    TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                // remove fake node
+                if (e.Node.Nodes.Count == 1 &&
+                    e.Node.Nodes[0].Text == "Loading...")
+                {
+                    e.Node.Nodes.Clear();
+
+                    string path = e.Node.Tag.ToString();
+
+                    foreach (string dir in Directory.GetDirectories(path))
+                    {
+                        TreeNode child =
+                            new TreeNode(Path.GetFileName(dir));
+
+                        child.Tag = dir;
+
+                        child.Nodes.Add("Loading...");
+
+                        e.Node.Nodes.Add(child);
+                    }
+                }
+            }
+            catch { }
+        }
+
         private void treeViewFolders_AfterSelect(object sender,
     TreeViewEventArgs e)
         {
@@ -120,7 +155,8 @@ namespace Lab13
             }
             if (ext == ".txt")
             {
-                richTextBoxInfo.Text = File.ReadAllText(path);
+                richTextBoxInfo.Text =
+                    File.ReadAllText(path);
             }
         }
         private void btnFilterFiles_Click(object sender, EventArgs e)
@@ -166,6 +202,336 @@ namespace Lab13
                     : Color.White;
 
                 FilterNodes(child, filter);
+            }
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (treeViewFolders.SelectedNode == null)
+                {
+                    MessageBox.Show("Select folder first");
+                    return;
+                }
+
+                string currentPath =
+                    treeViewFolders.SelectedNode.Tag.ToString();
+
+                string newFolder =
+                    Path.Combine(currentPath, txtNewName.Text);
+
+                Directory.CreateDirectory(newFolder);
+
+                MessageBox.Show("Folder created");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path =
+                    treeViewFolders.SelectedNode.Tag.ToString();
+
+                Directory.Delete(path, true);
+
+                MessageBox.Show("Folder deleted");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string source =
+                    treeViewFolders.SelectedNode.Tag.ToString();
+
+                FolderBrowserDialog fbd =
+                    new FolderBrowserDialog();
+
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    string destination =
+                        Path.Combine(
+                            fbd.SelectedPath,
+                            Path.GetFileName(source));
+
+                    CopyDirectory(source, destination);
+
+                    MessageBox.Show("Folder copied");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void CopyDirectory(string source, string dest)
+        {
+            Directory.CreateDirectory(dest);
+
+            foreach (string file in Directory.GetFiles(source))
+            {
+                string name = Path.GetFileName(file);
+
+                File.Copy(file,
+                    Path.Combine(dest, name), true);
+            }
+
+            foreach (string folder in Directory.GetDirectories(source))
+            {
+                string name = Path.GetFileName(folder);
+
+                CopyDirectory(folder,
+                    Path.Combine(dest, name));
+            }
+        }
+
+        private void moveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string source =
+                    treeViewFolders.SelectedNode.Tag.ToString();
+
+                FolderBrowserDialog fbd =
+                    new FolderBrowserDialog();
+
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    string destination =
+                        Path.Combine(
+                            fbd.SelectedPath,
+                            Path.GetFileName(source));
+
+                    Directory.Move(source, destination);
+
+                    MessageBox.Show("Folder moved");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void createToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path =
+                    treeViewFolders.SelectedNode.Tag.ToString();
+
+                string file =
+                    Path.Combine(path, txtNewName.Text);
+
+                File.Create(file).Close();
+
+                MessageBox.Show("File created");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void deleteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBoxFiles.SelectedItem == null)
+                    return;
+
+                string file =
+                    listBoxFiles.SelectedItem.ToString();
+
+                File.Delete(file);
+
+                MessageBox.Show("File deleted");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void copyToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBoxFiles.SelectedItem == null)
+                    return;
+
+                string source =
+                    listBoxFiles.SelectedItem.ToString();
+
+                FolderBrowserDialog fbd =
+                    new FolderBrowserDialog();
+
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    string destination =
+                        Path.Combine(
+                            fbd.SelectedPath,
+                            Path.GetFileName(source));
+
+                    File.Copy(source, destination, true);
+
+                    MessageBox.Show("File copied");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void moveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBoxFiles.SelectedItem == null)
+                    return;
+
+                string source =
+                    listBoxFiles.SelectedItem.ToString();
+
+                FolderBrowserDialog fbd =
+                    new FolderBrowserDialog();
+
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    string destination =
+                        Path.Combine(
+                            fbd.SelectedPath,
+                            Path.GetFileName(source));
+
+                    File.Move(source, destination);
+
+                    MessageBox.Show("File moved");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void changeAttributesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBoxFiles.SelectedItem == null)
+                {
+                    MessageBox.Show("Select a file first");
+                    return;
+                }
+
+                string file =
+                    listBoxFiles.SelectedItem.ToString();
+
+                FileAttributes attr =
+                    File.GetAttributes(file);
+
+                File.SetAttributes(file,
+                    attr | FileAttributes.Hidden);
+
+                MessageBox.Show("Hidden attribute added");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void saveTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string file =
+                    listBoxFiles.SelectedItem.ToString();
+
+                using (FileStream fs =
+                    new FileStream(file, FileMode.Create,
+                    FileAccess.Write, FileShare.None))
+                {
+                    using (StreamWriter writer =
+                        new StreamWriter(fs))
+                    {
+                        writer.Write(richTextBoxInfo.Text);
+                    }
+                }
+
+                MessageBox.Show("Saved successfully");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("No access to file. Run as Admin.");
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("File is being used by another process.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        private void zIPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string file =
+                    listBoxFiles.SelectedItem.ToString();
+
+                string zip =
+                    file + ".zip";
+
+                using (ZipArchive archive =
+                    ZipFile.Open(zip, ZipArchiveMode.Create))
+                {
+                    archive.CreateEntryFromFile(
+                        file,
+                        Path.GetFileName(file));
+                }
+
+                MessageBox.Show("Archived");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void uNZIPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string zip =
+                    listBoxFiles.SelectedItem.ToString();
+
+                string folder =
+                    Path.Combine(
+                        Path.GetDirectoryName(zip),
+                        "Extracted");
+
+                ZipFile.ExtractToDirectory(zip, folder);
+
+                MessageBox.Show("Extracted");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
